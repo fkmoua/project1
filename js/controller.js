b5a8e61484dfedd5672e7e3fad588fcf3f33dd6e
Your Name <mouafancy@gmail.com>
2015-04-03 10:03:21 -0700

    player one and player two both can play

--- js/controller.js
+++ js/controller.js
@@ -10,36 +10,50 @@
 
 
 		self.boxClicked = boxClicked;
-		self.makeMove = makeMove;
+		// self.makeMove = makeMove;
 
 		// setting the gameboard //
 		self.gameboard = [
-			{picked: false, alias: "a"},
-			{picked: false, alias: "b"},
-			{picked: false, alias: "c"},
-			{picked: false, alias: "d"},
-			{picked: false, alias: "e"},
-			{picked: false, alias: "f"},
-			{picked: false, alias: "g"},
-			{picked: false, alias: "h"},
-			{picked: false, alias: "i"}
+			{clicked_player_one: false, clicked_player_two: false, alias: "a"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "b"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "c"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "d"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "e"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "f"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "g"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "h"},
+			{clicked_player_one: false, clicked_player_two: false, alias: "i"}
 		];
 
 		// end of gameboard //
- 
+ 		var counter = 0;
+ 		// if ((self.gameboard[$index].clicked_player_one = false) && (self.gameboard[$index].clicked_player_two = false)) {
 		function boxClicked($index) {
-			var counter = 9;
-			self.gameboard[$index].picked = true; // Setting the 'picked' property in the gameboard to true.
-			
-		};
-
-		function makeMove($index) {
-			if (self.gameboard[$index] = true) {
-				alert("picked already!");
-			}			else {
-				console.log("TOMATOES!");
+	
+			if (counter % 2 === 0) {
+				self.gameboard[$index].clicked_player_one = true; // Setting the 'picked' property in the gameboard to true.	
+			} else if (counter % 2 != 0 ) {
+				self.gameboard[$index].clicked_player_two = true; // Setting the 'picked' property in the gameboard to true.	
+				// counter++;
+			} else {
+				alert("pick again!")
+				// counter++;
 			}
-		};
+			counter++;
+		}
+		
+
+		// function makeMove($index) {
+		// 	// var counter = 9;
+		// 	// for (var i = counter; i < counter.length; i--) {
+		// 	// 	console.log("hello world")
+		// 	// }
+		// 	// if (self.gameboard[$index] = true) {
+		// 	// 	alert("picked already!");
+		// 	// }			else {
+		// 	// 	console.log("TOMATOES!");
+		// 	// }
+		// };
 
 		
 		// function makeMove($index) {

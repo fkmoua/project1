af15ee7a53fb7aac0a2691a1250bc1423833cc7a
Your Name <mouafancy@gmail.com>
2015-04-03 17:06:15 -0700

    both player moves synced with Firebase

--- js/controller.js
+++ js/controller.js
@@ -1,66 +1,60 @@
  angular
 	.module('TicTacToe')
 	.controller('MainController', MainController);
-	counter = 0;
+	
+	MainController.$inject = ['$firebaseObject'];
 
-	function MainController() {
+	function MainController($firebaseObject) {
 		var self = this; 
-		var player1 = "X";
-		var player2 = "0";
 
+		// self.boxClicked = boxClicked;
+		self.syncFirebaseGameboard = syncFirebaseGameboard(); // essentially self.synchFirebasegameboard is gameObject;
 
 		self.boxClicked = boxClicked;
-		// self.makeMove = makeMove;
-
 		// setting the gameboard //
-		self.gameboard = [
-			{clicked_player_one: false, clicked_player_two: false, alias: "a"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "b"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "c"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "d"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "e"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "f"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "g"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "h"},
-			{clicked_player_one: false, clicked_player_two: false, alias: "i"}
-		];
+		
 
-		// end of gameboard //
- 		var counter = 0;
- 		// if ((self.gameboard[$index].clicked_player_one = false) && (self.gameboard[$index].clicked_player_two = false)) {
+		function syncFirebaseGameboard() {
+			var ref = new Firebase('http://fancy-tictactoe.firebaseio.com')
+			var gameObject = $firebaseObject(ref);
+
+			gameObject.$loaded(function() {
+				gameObject.counter = 0;
+				gameObject.gameboard = [
+					{clicked_player_one: false, clicked_player_two: false, alias: "a"},
+		            		{clicked_player_one: false, clicked_player_two: false, alias: "b"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "c"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "d"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "e"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "f"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "g"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "h"},
+				           {clicked_player_one: false, clicked_player_two: false, alias: "i"}
+				  ] // end of gameObject.gameboard
+				gameObject.$save()
+				} 
+
+			) // end of $loaded function.
+			return gameObject;
+		} // end of function syncFirebaseGameboard()
+		
 		function boxClicked($index) {
-	
-			if (counter % 2 === 0) {
-				self.gameboard[$index].clicked_player_one = true; // Setting the 'picked' property in the gameboard to true.	
-			} else if (counter % 2 != 0 ) {
-				self.gameboard[$index].clicked_player_two = true; // Setting the 'picked' property in the gameboard to true.	
-				// counter++;
+			console.log("hello world")
+			// self.syncFirebaseGameboard.gameboard[$index].clicked_player_one = false;
+			// self.syncFirebaseGameboard.gameboard[$index].clicked_player_one = false;
+			if (self.syncFirebaseGameboard.counter % 2 === 0) {
+				self.syncFirebaseGameboard.gameboard[$index].clicked_player_one = true; // Setting the 'picked' property in the gameboard to true.	
+				self.syncFirebaseGameboard.$save();
+			} else if (self.syncFirebaseGameboard.counter % 2 != 0 ) {
+				self.syncFirebaseGameboard.gameboard[$index].clicked_player_two = true; // Setting the 'picked' property in the gameboard to true.	
+				self.syncFirebaseGameboard.$save();
+				
 			} else {
 				alert("pick again!")
-				// counter++;
 			}
-			counter++;
-		}
-		
-
-		// function makeMove($index) {
-		// 	// var counter = 9;
-		// 	// for (var i = counter; i < counter.length; i--) {
-		// 	// 	console.log("hello world")
-		// 	// }
-		// 	// if (self.gameboard[$index] = true) {
-		// 	// 	alert("picked already!");
-		// 	// }			else {
-		// 	// 	console.log("TOMATOES!");
-		// 	// }
-		// };
-
-		
-		// function makeMove($index) {
-
-		// }
-
-		//self.gameboard[0].picked = false;
-	
+			self.syncFirebaseGameboard.counter++;
+			self.syncFirebaseGameboard.$save();
+			
+		} // end of boxClicked function
 	} //end of MainController function
 
